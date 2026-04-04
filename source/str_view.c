3eee6e434f4d77d41a707cce93c8a1d8a049528d
Alexander Lopez <agl.alexglopez@gmail.com>
2026-04-04 16:03:23 -0700

    make bool explicit

--- source/str_view.c
+++ source/str_view.c
@@ -819,8 +819,9 @@ char_compare(char const a, char const b) {
 
 static inline bool
 bitset_set(size_t *const bitset, size_t const char_as_size_t) {
-    return bitset[char_as_size_t / (8 * sizeof(*bitset))]
-        |= (size_t)(1 << (char_as_size_t % (8 * sizeof(*bitset))));
+    return (bitset[char_as_size_t / (8 * sizeof(*bitset))]
+            |= (size_t)(1 << (char_as_size_t % (8 * sizeof(*bitset)))))
+        != 0;
 }
 
 static inline bool
